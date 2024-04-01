6fbe4c6f1c4bc69779ec61aa0c5076f697e38876
Zihan Eric Tang <zhtangsh@163.com>
2024-04-01 11:48:24 +0800

    bugfix: :bug: fix requirement

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,2 @@
 SHORT_SHA=$(git rev-parse --short HEAD)
 docker build ../.. -f Dockerfile -t zhtangsh/paimon:dev.$SHORT_SHA
-docker tag zhtangsh/paimon:dev.$SHORT_SHA 192.168.1.50:29006/zhtangsh/paimon:dev.$SHORT_SHA
-docker push 192.168.1.50:29006/zhtangsh/paimon:dev.$SHORT_SHA
